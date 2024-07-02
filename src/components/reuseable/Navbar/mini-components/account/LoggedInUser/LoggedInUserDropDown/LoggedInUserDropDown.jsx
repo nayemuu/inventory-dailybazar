85d769877ad9fbbc084b19f1f959f8a4586130f4
Nayem-Hasan <nayeemhasanpr@gmail.com>
2024-07-02 21:18:12 +0600

    logged in user dropdown modified

--- src/components/reuseable/Navbar/mini-components/account/LoggedInUser/LoggedInUserDropDown/LoggedInUserDropDown.jsx
+++ src/components/reuseable/Navbar/mini-components/account/LoggedInUser/LoggedInUserDropDown/LoggedInUserDropDown.jsx
@@ -3,13 +3,13 @@ import { SlLogout } from 'react-icons/sl';
 const LoggedInUserDropDown = ({ userDropdown }) => {
   return (
     <div
-      className={`absolute top-[45px] right-0 drop-shadow-[0_2px_4px_rgb(0,0,0,0.08)] bg-white p-5 border border-[#e6e6e6] rounded-[12px]`}
+      className={`absolute top-[45px] right-0 drop-shadow-[0_2px_4px_rgb(0,0,0,0.08)] bg-white py-4 border border-[#e6e6e6] rounded-[12px]`}
     >
-      <div className="flex gap-4 items-center px-3 py-2 cursor-pointer font-medium hover:bg-primary hover:text-white transition-all duration-200 ease-linear">
+      <div className="flex gap-4 items-center pl-[30px] pr-[40px] py-2 cursor-pointer hover:bg-primary hover:text-white transition-all duration-200 ease-linear">
         <div className="text-lg">
           <SlLogout />
         </div>
-        <div className="whitespace-nowrap">Log Out</div>
+        <div className="whitespace-nowrap text-[16px] font-medium">Log Out</div>
       </div>
     </div>
   );
