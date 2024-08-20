d2c1962474cd5ef73c8ff55c0382ecd74453f003
Nayeem-Hasan <hasan@care-box.com>
2024-08-20 16:16:53 +0600

    working on pagination

--- src/components/pages/Test/TestTable/TestTable.jsx
+++ src/components/pages/Test/TestTable/TestTable.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
-import CommonSectionTable from '../../../reuseable/Section/CommonSectionTable/CommonSectionTable';
+import { useState } from "react";
+import CommonSectionTable from "../../../reuseable/Section/CommonSectionTable/CommonSectionTable";
+import ReactPaginate from "react-paginate";
+import JumpToPageButton from "../../../reuseable/buttons/JumpToPageButton/JumpToPageButton";
 
 function TestTable({ data }) {
   const [selectedIds, setSelectedIds] = useState([]);
 
   // console.log('selectedIds = ', selectedIds);
 
-  let headers = ['Category Id', 'Category Name', 'Location'];
-  let fieldToShow = ['id', 'category', 'location'];
+  let headers = ["Category Id", "Category Name", "Location"];
+  let fieldToShow = ["id", "category", "location"];
   let actionButtons = {
     view: true,
     edit: true,
@@ -38,15 +40,15 @@ function TestTable({ data }) {
   };
 
   const handleView = (id) => {
-    console.log('id = ', id);
+    console.log("id = ", id);
   };
 
   const handleEdit = (id) => {
-    console.log('id = ', id);
+    console.log("id = ", id);
   };
 
   const handleDelete = (id) => {
-    console.log('id = ', id);
+    console.log("id = ", id);
   };
 
   return (
@@ -65,6 +67,43 @@ function TestTable({ data }) {
         handleEdit={handleEdit}
         handleDelete={handleDelete}
       />
+
+      <div className="pt-[50px] flex justify-between items-center gap-5">
+        <div>
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=""
+            onPageChange={() => {}} // define a onPageChange handler
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={1}
+            pageCount={5} // total number of page
+            initialPage={1} // Pass the initialPage state
+            previousLabel=""
+            renderOnZeroPageCount={null}
+            containerClassName="flex flex-wrap gap-x-[10px] justify-center"
+            pageLinkClassName="unselected-page"
+            activeLinkClassName="active-page"
+            previousClassName="hide"
+            nextClassName="hide"
+          />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <div className="text-[16px] leading-[18px] font-semibold text-primary">
+            Jump to page
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="page no."
+              className="w-[80px] h-[34px] rounded-[3px] flex justify-center items-center border border-primary text-center px-1"
+            />
+          </div>
+          <div className="w-[150px]">
+            <JumpToPageButton isLoading={false}>Jump</JumpToPageButton>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
